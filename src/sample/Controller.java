648e75308622142f9bebee48007c6d3a879a4ec9
greeneye256 <radusimu@gmail.com>
2019-12-12 08:06:43 +0200

    ver2

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -6,12 +6,17 @@ import javafx.scene.control.TextArea;
 
 public class Controller {
     private String operator = null;
-    char sign = '+';
     private String retainedOperator = null;
+    boolean equalsIsActive = false;
     private double calcResult = 0;
     public TextArea result;
 
     public void numberPressed(ActionEvent event) {
+        if (equalsIsActive) {
+            calcResult = 0;
+            equalsIsActive = false;
+            result.setText("0");
+        }
         Button button = (Button) event.getSource();
         if (button.getText().equals(".")) {
             if (result.getText().contains(button.getText())) {
@@ -20,7 +25,6 @@ public class Controller {
             result.setText(result.getText() + ".");
             return;
         }
-
         if (operator == null) {
             if (result.getText().length() < 13) {
                 StringBuilder stringBuilder = new StringBuilder();
@@ -51,6 +55,22 @@ public class Controller {
                 retainedOperator = null;
                 result.setText("0");
                 break;
+            case "CE":
+                if (operator != null) {
+                    retainedOperator = operator;
+                    operator = null;
+                }
+                result.setText("0");
+                break;
+            case "\u232b":
+                if (operator == null) {
+                    if (result.getText().length() == 1) {
+                        result.setText("0");
+                    } else {
+                        result.setText(result.getText().substring(0, result.getText().length() - 1));
+                    }
+                }
+                return;
             case "1/x":
                 currentResult = 1 / Double.parseDouble(result.getText());
                 break;
@@ -61,12 +81,12 @@ public class Controller {
                 currentResult = Math.sqrt(Double.parseDouble(result.getText()));
                 break;
             case "%":
-                if (operator != null) {
-                    currentResult = Double.parseDouble(result.getText()) * 100 / calcResult;
+                if (operator == null) {
+                    currentResult = (calcResult * Double.parseDouble(result.getText())) / 100;
                 }
                 break;
             case "-/+":
-                currentResult = - Double.parseDouble(result.getText());
+                currentResult = -Double.parseDouble(result.getText());
                 break;
 
         }
@@ -77,11 +97,11 @@ public class Controller {
     private String beautifyDouble(Double number) {
         String myStringNumber = String.valueOf(number);
         if (myStringNumber.contains(".")) {
-            if (myStringNumber.indexOf('.') > 11) {
+            if (myStringNumber.indexOf('.') > 12) {
                 return "Error. Press C";
             } else {
-                if (number > number.intValue()) {
-                    return myStringNumber.substring(0, 11);
+                if (number != number.intValue()) {
+                    return (myStringNumber.length() > 12) ? myStringNumber.substring(0, 12) : myStringNumber;
                 } else {
                     return String.valueOf(number.intValue());
                 }
@@ -94,27 +114,53 @@ public class Controller {
         return myStringNumber;
     }
 
+    //throw arritmetic exception
+    public Double calculate(String operator,Double d1,Double d2){
+        switch (operator){
+            case "+":
+                return d1+d2;
+            case "-":
+                return d1-d2;
+            case "/":
+                return d1/d2;
+            case "*":
+                return d1*d2;
+        }
+        return 0d;
+    }
+
     public void operate(ActionEvent event) {
         Button button = (Button) event.getSource();
         operator = button.getText();
+
         if (retainedOperator == null) {
             calcResult = Double.parseDouble(result.getText());
         } else {
             switch (retainedOperator) {
                 case "+":
+                    equalsIsActive = false;
                     calcResult = calcResult + Double.parseDouble(result.getText());
                     break;
                 case "-":
+                    equalsIsActive = false;
                     calcResult = calcResult - Double.parseDouble(result.getText());
                     break;
                 case "*":
+                    equalsIsActive = false;
                     calcResult = calcResult * Double.parseDouble(result.getText());
                     break;
                 case "/":
+                    equalsIsActive = false;
                     calcResult = calcResult / Double.parseDouble(result.getText());
                     break;
-                case "=":
-                    break;
+            }
+            if (operator.equals("=")){
+                equalsIsActive = true;
+                result.setText(beautifyDouble(calcResult));
+                calcResult = 0;
+                operator = null;
+                retainedOperator = null;
+                return;
             }
             result.setText(beautifyDouble(calcResult));
             retainedOperator = null;
